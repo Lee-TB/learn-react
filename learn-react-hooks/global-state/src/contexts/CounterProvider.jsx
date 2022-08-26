f17ee390ff2a4bedca43a14092e6db0afd9c3d6e
Lee TB <tblyduc2412@gmail.com>
2022-08-26 17:18:15 +0700

    learn-react-hooks/global-state global state with useContext + useReducer

---
+++ learn-react-hooks/global-state/src/contexts/CounterProvider.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useReducer } from 'react';
+
+const CounterContext = React.createContext();
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+function CounterProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CounterContext.Provider value={[state, dispatch]}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+
+const useCounterContext = () => {
+  return useContext(CounterContext);
+};
+
+export { useCounterContext };
+
+export default CounterProvider;
